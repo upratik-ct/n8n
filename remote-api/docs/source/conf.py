28968693511d5ec2120d1dedef547818b057a127
Pratik Upadhye <pratik.upadhye@cointab.net>
2025-07-31 05:07:59 +0000

    Add remote "/remote-api/"
    remoteId: rem_01k1fe2csdgacj7z151s3sp58e
    remoteUpdateId: grupd_01k1fe2ehskxajgrpxw5gk1yrp
    branchName: main
    commitSha: 3627a34bf3468e03b69945286a3e04ffe1721a10
    commitUrl: https://github.com/upratik-ct/markitdown/commit/3627a34bf3468e03b69945286a3e04ffe1721a10

---
+++ remote-api/docs/source/conf.py
@@ -0,0 +1,15 @@
+project = 'MarkitDown'
+copyright = ''
+author = 'upratik-ct'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
---
+++ remote-api/docs/source/index.rst
@@ -0,0 +1,8 @@
+Welcome to MarkitDown's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+This is a placeholder for your documentation.
